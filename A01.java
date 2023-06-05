46da1a4742bce969427fa1024333c8aea5b3fd7a
koki1357 <90707936+koki1357@users.noreply.github.com>
2023-06-06 00:51:20 +0900

    Create A01.java

---
+++ A01.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+	public static void main(String[] args) {
+      //「System.in」で標準入力
+      Scanner sc = new Scanner(System.in);
+      //nextInt（）をで数字として受け取る
+      int N = sc.nextInt();
+      //標準出力
+      System.out.println(N * N);
+    }
+}
